33a410bcd23fce0aaacf1e086c5dc4976894fef8
RAJARATHINAM-S <rajarathinams.mech@gmail.com>
2021-09-10 19:28:33 +0530

    Wages added

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //  UC1_EmployeeAttendance.GetAttendance();
+          // UC2_EmpWage.EmpWage();
+           // UC3_PartTimeEmpWage.PartTimeEmpWage();
+           // UC4_EmpWageUsingSwitch.EmpWageUsingSwitch();
+           // UC5_CalculateWagesFor20DaysInMonth.CalculateWagesFor20DaysInMonth();
+           // UC6_CalculateWageTill100HrsOr20IsReached.CalculateWageTill100HrsOr20IsReached();
+           // UC7_ComputeEmpWageUsingClassMethod.ComputeEmpWage();
+            UC8_ComputeEmpWageForMultipleCompanies.ComputeEmpWage("mnc", 20, 24, 150);
+
+
+            Console.ReadLine();
+        }
+    }
+}
