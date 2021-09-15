94b00ffcaabc795a7450d879006fca6d3b664ffe
RAJARATHINAM-S <rajarathinams.mech@gmail.com>
2021-09-15 10:26:28 +0530

    final solution

---
+++ UC_LastFinalSolution/IComputeEmpWage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4.UC_LastFinalSolution
+{
+    interface IComputeEmpWage
+    {
+        void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMnth);
+        void computeEmpWage();
+
+        int getTotalWage(string company); 
+    }
+}
